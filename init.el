aa4c7bef0b3d8b5bf61e220818dc66e5aa6736c3
Ln Gnmn <lngnmn2@yahoo.com>
2023-08-25 07:21:26 +0545

    init

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
-;;; init.el -*- lexical-binding: t; coding: utf-8; -*-
+;;; -*- mode: emacs-lisp; lexical-binding: t; coding: utf-8; -*-
 
 (setq-default load-prefer-newer t)
+
+;; temporary kludge
+(setq-default user-emacs-directory "/home/lngnmn2/.nano-emacs.d/")
 
 (setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
       gc-cons-percentage 0.6)
@@ -16,13 +19,31 @@
 
 ;; we use straight.el
 (setq package-enable-at-startup nil)
+(setq package-quickstart nil)
 
-;; proper encodings for ore-babel
+;; proper encodings for org-babel
+;;
+(setenv "LC_CTYPE" "UTF-8")
+(setenv "LC_ALL" "en_US.UTF-8")
+(setenv "LANG" "en_US.UTF-8")
+
 (set-charset-priority 'unicode)
 (set-language-environment 'UTF-8)
-(prefer-coding-system 'utf-8-unix)
-(set-default-coding-systems 'utf-8-unix)
-(set-terminal-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8)
+
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+
+(setq locale-coding-system 'utf-8
+      coding-system-for-read 'utf-8
+      coding-system-for-write 'utf-8)
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
+
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 
 ;; We use the 'straight bloatware
 (defvar bootstrap-version)
@@ -31,30 +52,43 @@
       (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-		(url-retrieve-synchronously
-		 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-	     'silent 'inhibit-cookies)
+                (url-retrieve-synchronously
+                 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+             'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;; use-package will use 'straight
-(straight-use-package 'diminish)
-(straight-use-package '(use-package :type built-in))
+(setq package-enable-at-startup nil)
 
-;; Use the latest version
-(straight-use-package 'org)
+(setq straight-use-package-by-default t)
+(setq straight-vc-git-default-clone-depth 1)
 
 ;; we will use this DSLs (a set of macros)
-(setq-default
- use-package-always-defer nil   ;; should be nil for :defer to work
- use-package-always-ensure t    ;; should be t for straight
- use-package-compute-statistics t
- use-package-verbose t)
+(setq use-package-always-defer t
+      use-package-always-ensure t
+      use-package-compute-statistics t
+      use-package-verbose t)
+
+;; use-package will use 'straight
+(straight-use-package '(use-package :type built-in))
+(straight-use-package '(bind-key :type built-in))
+(straight-use-package 'diminish)
+(straight-use-package 'delight)
 
 (straight-use-package 'async)
-(straight-use-package 'ob-async)
+(use-package ob-async
+  :hook (org-load . (lambda () (require 'ob-async))))
 
+(menu-bar-mode t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(fringe-mode -1)
+
+;; Use the latest version instead of the bundled one
+(straight-use-package 'org)
+
+;; stolen hacks from Doom Emacs
 (defun doom-unquote (exp)
   "Return EXP unquoted."
   (declare (pure t) (side-effect-free t))
@@ -89,10 +123,11 @@ advised)."
               (add-hook sym #',fn ,append?))))))
 
 (add-transient-hook! #'org-babel-execute-src-block
-					 (require 'ob-async))
+  (require 'ob-async))
 
 ;; and a "literate" org-mode configuration
-(setf config-org (expand-file-name "~/.emacs.d/config.org"))
+(setf config-org (expand-file-name "~/.nano-emacs.d/config.org"))
 (when (file-readable-p config-org)
   (org-babel-load-file config-org))
 
+;;; init.el ends here
